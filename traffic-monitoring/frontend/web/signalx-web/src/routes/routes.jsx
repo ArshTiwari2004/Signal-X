b259f9cc8c7d96a2513138a3f9085e4ffb9549dc
Priyanshi Bothra <priyanshibothra753@gmail.com>
2025-04-05 02:34:30 +0530

    feat : add report blockage in mobile

--- traffic-monitoring/frontend/web/signalx-web/src/routes/routes.jsx
+++ traffic-monitoring/frontend/web/signalx-web/src/routes/routes.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../components/Home';
 import Dashboard from '../pages/Dashboard';
-import DashboardLayout from '../layout/Layout';
+import DashboardLayout from '../layout/layout';
 import TrafficMap from '../pages/Trafficmap';
 import Monitoring from '../pages/Livemonitoring';
 import LiveMonitoring from '../pages/Livemonitoring';
